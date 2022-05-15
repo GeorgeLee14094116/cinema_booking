7d7dd2cfa36ecf5e438f7d7d4ef984ada438a61a
GeorgeLee14094116 <105350187+GeorgeLee14094116@users.noreply.github.com>
2022-05-15 20:58:12 +1000

    Add files via upload

---
+++ assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment3
+//
+//  Created by George Lee on 12/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
